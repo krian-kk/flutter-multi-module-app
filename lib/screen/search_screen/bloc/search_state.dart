e7c38d2849ee8a1ab034cf4b1bf85ccb76e8c3a6
Nandha Kumar <nandhakumar@Jacks-MacBook-Pro-2.local>
2021-11-22 15:27:59 +0530

    search ui error fixed

---
+++ lib/screen/search_screen/bloc/search_state.dart
@@ -0,0 +1,18 @@
+part of 'search_bloc.dart';
+
+@immutable
+class SearchScreenState extends BaseEquatable {}
+
+class SearchScreenInitial extends SearchScreenState {}
+
+class SearchScreenInitialState extends SearchScreenState {
+}
+
+class SearchScreenLoadingState extends SearchScreenState {
+}
+
+class SearchScreenLoadedState extends SearchScreenState {
+}
+
+// class ShowPincodeInAllocationState extends SearchScreenState {
+// }
