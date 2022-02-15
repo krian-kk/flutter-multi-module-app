d9096448b0947a9e2a7194248aa265d9b0d393f2
chinnaduraiv@live.in <chinna@m2p.in>
2022-02-15 13:12:19 +0530

    FCM integration added in main screen

--- lib/main.dart
+++ lib/main.dart
@@ -10,14 +10,13 @@ import 'package:origa/languages/app_localizations_delegate.dart';
 import 'package:origa/router.dart';
 import 'package:origa/screen/splash_screen/splash_screen.dart';
 import 'package:origa/utils/app_theme.dart';
+import 'package:overlay_support/overlay_support.dart';
 
 import 'authentication/authentication_bloc.dart';
 import 'authentication/authentication_event.dart';
 import 'bloc.dart';
 
 void main() async {
-  //Android //fiEaVaAARcuPY9m3M8nC4j:APA91bFr42QhThEzJZf4AvJjll8mV6pB59pMwSW_tu9mkSdDJzTu1nrkclyh1J_WJPdujmzf0ixFJdN25eJvEZp2hX96Fg9x0Iw7pJRbAyOPzzrFgA3tTUqD4GkLSuJSnORvTX_PgraR
-  //iOS //eex9QKjyVkQdqd95EItHgp:APA91bF3W606FW5ZO9bldHYSCBhoZXex4z0TD24IRqy0840xBZ8YIw-2E-yib1Dg3fAwQDXfRaT6UfOcX-Zu8J3LhOzGVQ5TyyVFruZlEKofKXj0ddmGO9cb3qNyO3Orrpsj6GI_EGuQ
   Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
     debugPrint('Handling a background message ID ${message.messageId}');
     debugPrint(
@@ -61,11 +60,11 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     bloc = BlocProvider.of<AuthenticationBloc>(context);
-    super.initState();
-    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
-      debugPrint(
-          'When the app opened via clicked notification--> ${message.notification!.title}');
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      debugPrint('Notification print-> ${message.notification!.title}');
+      debugPrint('notification!.body-> ${message.notification!.body}');
     });
+    super.initState();
   }
 
   void setLocale(Locale locale) {
@@ -89,34 +88,36 @@ class _MyAppState extends State<MyApp> {
     return DynamicTheme(
       themeCollection: AppThemes().getThemeCollections(),
       builder: (BuildContext context, ThemeData theme) {
-        return MaterialApp(
-          locale: _locale,
-          supportedLocales: const [
-            Locale('en', ''),
-            Locale('hi', ''),
-            Locale('ta', '')
-          ],
-          localizationsDelegates: const [
-            AppLocalizationsDelegate(),
-            GlobalMaterialLocalizations.delegate,
-            GlobalWidgetsLocalizations.delegate,
-            GlobalCupertinoLocalizations.delegate
-          ],
-          localeResolutionCallback:
-              (Locale? locale, Iterable<Locale> supportedLocales) {
-            for (Locale supportedLocale in supportedLocales) {
-              if (supportedLocale.languageCode == locale?.languageCode &&
-                  supportedLocale.countryCode == locale?.countryCode) {
-                return supportedLocale;
+        return OverlaySupport.global(
+          child: MaterialApp(
+            locale: _locale,
+            supportedLocales: const [
+              Locale('en', ''),
+              Locale('hi', ''),
+              Locale('ta', '')
+            ],
+            localizationsDelegates: const [
+              AppLocalizationsDelegate(),
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate
+            ],
+            localeResolutionCallback:
+                (Locale? locale, Iterable<Locale> supportedLocales) {
+              for (Locale supportedLocale in supportedLocales) {
+                if (supportedLocale.languageCode == locale?.languageCode &&
+                    supportedLocale.countryCode == locale?.countryCode) {
+                  return supportedLocale;
+                }
               }
-            }
-            return supportedLocales.first;
-          },
-          onGenerateRoute: getRoute,
-          debugShowCheckedModeBanner: false,
-          home: addAuthBloc(
-            context,
-            const SplashScreen(),
+              return supportedLocales.first;
+            },
+            onGenerateRoute: getRoute,
+            debugShowCheckedModeBanner: false,
+            home: addAuthBloc(
+              context,
+              const SplashScreen(),
+            ),
           ),
         );
       },
