6a98e4e2203b9d96b3742918ed5665ec66bbb427
chinnaduraiv@live.in <chinna@m2p.in>
2022-02-15 22:41:42 +0530

    FCM integration added in main screen

--- android/app/src/main/kotlin/com/example/origa/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/origa/MainActivity.kt
@@ -1,6 +1,19 @@
 package com.example.origa
 
+import android.os.Bundle
+import android.view.WindowManager
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        window.setFlags(
+//            WindowManager.LayoutParams.FLAG_SECURE,
+//            WindowManager.LayoutParams.FLAG_SECURE
+//        )
+        this.window.setFlags(
+            WindowManager.LayoutParams.FLAG_SECURE,
+            WindowManager.LayoutParams.FLAG_SECURE)
+    }
 }
+
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -31,14 +31,15 @@ import FirebaseMessaging
     }
     application.registerForRemoteNotifications()
     Messaging.messaging().delegate = self
-//    Messaging.messaging().token { token, error in
-//                  if let error = error {
-//                      print("Error fetching FCM registration token: \(error)")
-//                  } else if let token = token {
-//                      print("FCM registration token: \(token)")
-//                }
-//    }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+    
+override func applicationWillResignActive(_ application: UIApplication) {
+    self.window.isHidden = true;
+  }
+  override func applicationDidBecomeActive(_ application: UIApplication) {
+    self.window.isHidden = false;
   }
 }
 
