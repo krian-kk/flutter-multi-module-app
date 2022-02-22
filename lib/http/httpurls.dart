46d0a00002654f60c90140d8bd7df6f626ad3572
chinnaduraiv@live.in <chinna@m2p.in>
2022-02-22 11:19:53 +0530

    Live server pointed https://uat-collect.origa.ai

--- lib/http/httpurls.dart
+++ lib/http/httpurls.dart
@@ -1,6 +1,7 @@
 class HttpUrl {
-  static String url = 'https://collect.origa.ai';
-  // static String url = 'https://uat-collect.origa.ai';
+  // 1st - Customer will be reject ->
+  // static String url = 'https://collect.origa.ai';
+  static String url = 'https://uat-collect.origa.ai';
   // static String url = 'https://collect.instalmint.com';
 
   // static String? url = Singleton.instance.baseUrl;
