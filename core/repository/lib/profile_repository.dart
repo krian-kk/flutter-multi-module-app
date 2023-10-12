d3cc9cb7b47b8a6e1e39c81b704ca7a3fba07a49
krian_kk <krishnakant.chouhan@m2pfintech.com>
2023-10-12 16:13:38 +0530

    RD-1303 profile user type is stored in preference

--- core/repository/lib/profile_repository.dart
+++ core/repository/lib/profile_repository.dart
@@ -1,10 +1,13 @@
 import 'dart:io';
+
+import 'package:domain_models/common/constants.dart';
 import 'package:domain_models/response_models/profile/profile_data.dart';
 import 'package:network_helper/api_services/profile_api_service.dart';
+import 'package:network_helper/errors/network_exception.dart';
+import 'package:network_helper/network_base_models/api_result.dart';
 import 'package:network_helper/network_base_models/base_response.dart';
 import 'package:preference_helper/preference_constants.dart';
 import 'package:preference_helper/preference_helper.dart';
-import 'package:network_helper/network_base_models/api_result.dart';
 import 'package:repository/repo_utils.dart';
 
 abstract class ProfileRepository {
